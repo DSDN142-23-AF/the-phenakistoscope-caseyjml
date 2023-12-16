a4c8428161cdbcd4c6e6dac8183443244fd7e00f
caseyjml <151981877+caseyjml@users.noreply.github.com>
2023-12-17 11:27:39 +1300

    Update my_phenakistoscope.js
    output gif

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,7 +1,7 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
