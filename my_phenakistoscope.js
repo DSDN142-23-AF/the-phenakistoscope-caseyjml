5b791bf05e9ef7f6ddb29c25d0604c562b4f4c84
caseyjml <151981877+caseyjml@users.noreply.github.com>
2024-01-11 14:42:05 +1300

    rabbit animation
    updated rabbit + imported more images

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,31 +1,32 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(STATIC_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 
-  pScope.load_image_sequence("rabbit", "png", 2);
+  pScope.load_image_sequence("rabbit", "png", 3);
   pScope.load_image("cloud", "png");
+  pScope.load_image("grass1", "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 220);  // circle background
 
   var layer1 = new PLayer(sky);
   layer1.mode( RING );
   layer1.set_boundary( 0, 2000 );
 
   var layer2 = new PLayer(dandelions);
-  layer2.mode( SWIRL(5) );
+  layer2.mode( SWIRL(10) );
   layer2.set_boundary( 0, 2000 );
 
   var layer3 = new PLayer(centre);
   layer3.mode( RING );
-  layer3.set_boundary( 0, 400 );
+  layer3.set_boundary( 0, 2000 );
 
   var layer4 = new PLayer(rabbit);
   layer4.mode(RING);
@@ -61,28 +62,24 @@ function sky(x, y, animation, pScope){
 
 function dandelions(x, y, animation, pScope){
 scale(animation.frame*2);
-ellipse(0,0,150,150); // cloud placeholder
+ellipse(0,0,100,100); // dandelion placeholder
 
 }
 
+
 function centre(x, y, animation, pScope){
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
+  scale(.4)
+  pScope.draw_image("grass1", 0, 0)
 
-  scale(1.2);
-  fill(145, 222, 135)
-  arc(x,y,600,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
+  scale(1)
   fill(255) // gatsby
-  ellipse(-50,-300-animation.wave()*50,60,40) // .wave is a cosine wave btw
+  ellipse(-50,-300-animation.wave()*50,60,40) 
   fill(1)
   ellipse(-70,-300-animation.wave()*50,10,10)
 
   fill(150) // astro
-  ellipse(50,-300-animation.wave()*50,60,40) // .wave is a cosine wave btw
+  ellipse(50,-300-animation.wave()*50,60,40) 
   fill(1)
   ellipse(70,-300-animation.wave()*50,10,10)
 
@@ -90,8 +87,8 @@ function centre(x, y, animation, pScope){
 
 function rabbit(x, y, animation, pScope){
 
-  scale(.4);
-  pScope.draw_image_from_sequence("rabbit", 0, -1500, animation.frame) // beef
+  scale(.35);
+  pScope.draw_image_from_sequence("rabbit", 0, -1800, animation.frame) // beef
 }
 
 function clouds(x, y, animation, pScope){
